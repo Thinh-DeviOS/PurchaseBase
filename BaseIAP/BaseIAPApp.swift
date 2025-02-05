91f3e8b783518552577bbfad91fdf7e548565777
Thinh-DeviOS <thinhnd@infinitytech.global>
2025-02-05 10:05:06 +0700

    Initial Commit

---
+++ BaseIAP/BaseIAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseIAPApp.swift
+//  BaseIAP
+//
+//  Created by Nguyen Duc Thinh on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BaseIAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseIAP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaseIAP
+//
+//  Created by Nguyen Duc Thinh on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
